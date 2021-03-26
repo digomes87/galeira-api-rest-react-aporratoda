00ce95bf8ae670640ad3f29733cce7c8b8cea972
Diego Go <Diego.Gomes87@gmail.com>
2021-03-26 14:23:12 -0300

    fix error in settings

--- BackEnd/api/settings.py
+++ BackEnd/api/settings.py
@@ -74,13 +74,12 @@ DATABASES = {
     }
 }
 
-
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS':  (
-        "django.filters.rest_framework.DjangoFilterBackend",
-        "rest_framework.filter.SerchFilter",
+    'DEFAULT_FILTER_BACKENDS': (
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
         "rest_framework.filters.OrderingFilter",
-      ),
+    ),
 }
 
 
